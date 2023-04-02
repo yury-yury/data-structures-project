b7e94cb534bc7eb70b83c5e902d204a0f3480c9d
yury-yury <yury_yury@mail.ru>
2023-04-02 19:13:22 +0300

    end 1 task

--- src/stack.py
+++ src/stack.py
@@ -7,7 +7,8 @@ class Node:
 
         :param data: данные, которые будут храниться в узле
         """
-        pass
+        self.data = data
+        self.next_node = next_node
 
 
 class Stack:
@@ -15,7 +16,7 @@ class Stack:
 
     def __init__(self):
         """Конструктор класса Stack"""
-        pass
+        self.storage = []
 
     def push(self, data):
         """
@@ -23,7 +24,12 @@ class Stack:
 
         :param data: данные, которые будут добавлены на вершину стека
         """
-        pass
+        if len(self.storage) == 0:
+            previous = None
+        else:
+            previous = self.storage[-1]
+        current_value = Node(data, previous)
+        self.storage.append(current_value)
 
     def pop(self):
         """
@@ -31,4 +37,9 @@ class Stack:
 
         :return: данные удаленного элемента
         """
-        pass
+        return self.storage.pop()
+
+    @property
+    def top(self):
+        return self.storage[-1]
+
--- tests/test_stack.py
+++ tests/test_stack.py
@@ -1,1 +1,33 @@
-"""Здесь надо написать тесты с использованием unittest для модуля stack."""
+from unittest import TestCase, main
+from src.stack import Node, Stack
+
+
+class TestStack(TestCase):
+
+    def test_node(self):
+        n1 = Node(5, None)
+        n2 = Node('a', n1)
+        self.assertEqual(n1.data, 5)
+        self.assertEqual(n2.data, "a")
+        self.assertEqual(n2.next_node, n1)
+        self.assertEqual(n2.next_node.data, n1.data)
+
+
+    def test_slice(self):
+        stack = Stack()
+        stack.push('data1')
+        stack.push('data2')
+        stack.push('data3')
+        stack.push('data4')
+        self.assertEqual(stack.top.data, "data4")
+        self.assertEqual(stack.pop().data, "data4")
+        self.assertEqual(stack.top.data, "data3")
+        self.assertEqual(stack.top.next_node.data, "data2")
+        self.assertEqual(stack.top.next_node.next_node.data, "data1")
+        self.assertEqual(stack.top.next_node.next_node.next_node, None)
+        with self.assertRaises(AttributeError) as e:
+            res = stack.top.next_node.next_node.next_node.data
+
+
+if __name__ == '__main__':
+    main()
